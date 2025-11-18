19abbc150451ec896426970f4ffa057766c77ac7
Anjali Agarwal <anjaliagarwal@Anjalis-MacBook-Air.local>
2025-11-18 12:30:44 +0530

    Fix employee and checkout pages: Restore correct versions from merge commit
    - Restore EmployeeDashboard.tsx (11KB) - was corrupted (83B)
    - Restore CustomerSelection.tsx - was corrupted
    - Restore CheckoutSummary.tsx - was corrupted
    - Restore AppBenefits.tsx
    - All pages now properly restored from commit 57be673

--- src/pages/CustomerSelection.tsx
+++
@@ -1,1 +0,0 @@
-fatal: path 'src/pages/CustomerSelection.tsx' exists on disk, but not in 'be00e92'
--- src/pages/EmployeeDashboard.tsx
+++
@@ -1,1 +0,0 @@
-fatal: path 'src/pages/EmployeeDashboard.tsx' exists on disk, but not in 'be00e92'
